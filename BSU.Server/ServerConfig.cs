c561da1585bfbac28b95f7c8759bd09b46744ce1
Keithen <Keithen.Neu@gmail.com>
2019-12-25 12:31:40 +0100

    improved server usability a little

--- BSU.Server/ServerConfig.cs
+++ BSU.Server/ServerConfig.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using BSU.BSO.FileStructures;
+using BSU.Hashes;
 using IniParser;
+using IniParser.Model;
 
 namespace BSU.Server
 {
@@ -14,27 +17,43 @@ namespace BSU.Server
 
         public static ServerConfig Load(string path)
         {
+            if (!File.Exists(path))
+            {
+                Console.WriteLine("File not found: " + path);
+                throw new FileNotFoundException(path);
+            }
+
             var data = new FileIniDataParser().ReadFile(path);
             var server = new ServerFile
             {
-                ServerName = data["Server"]["Name"],
-                ServerAddress = data["Server"]["Address"],
-                Password = data["Server"]["Password"],
-                ServerPort = int.Parse(data["Server"]["Port"]),
-                LastUpdateDate = DateTime.Parse(data["Server"]["LastUpdate"]),
-                ServerGuid = Guid.Parse(data["Server"]["Guid"]),
-                SyncUris = data["Server"]["SyncUris"].Split(",").Select(u => new Uri(u)).ToList(),
-                CreationDate = DateTime.Parse(data["Server"]["CreationDate"]),
+                ServerName = data.GetData("Server", "Name"),
+                ServerAddress = data.GetData("Server", "Address"),
+                Password = data.GetData("Server", "Password"),
+                ServerPort = int.Parse(data.GetData("Server", "Port")),
+                LastUpdateDate = DateTime.Parse(data.GetData("Server", "LastUpdate")),
+                ServerGuid = Guid.Parse(data.GetData("Server", "Guid")),
+                SyncUris = data.GetData("Server", "SyncUris").Split(",").Select(u => new Uri(u)).ToList(),
+                CreationDate = DateTime.Parse(data.GetData("Server", "CreationDate")),
                 ModFolders = new List<ModFolder>()
             };
             return new ServerConfig
             {
-                ServerFileName = data["Misc"]["ServerFileName"],
-                SourcePath = data["Preset"]["SourcePath"],
-                TargetPath = data["Preset"]["TargetPath"],
-                ModList = data["Preset"]["ModList"],
+                ServerFileName = data.GetData("Misc", "ServerFileName"),
+                SourcePath = data.GetData("Preset", "SourcePath"),
+                TargetPath = data.GetData("Preset", "TargetPath"),
+                ModList = data.GetData("Preset", "ModList"),
                 Server = server
             };
+        }
+    }
+
+    static class IniHelper
+    {
+        public static string GetData(this IniData data, string section, string key)
+        {
+            if (data.TryGetKey(section + data.SectionKeySeparator + key, out var value)) return value;
+            Console.WriteLine($"Missing ini entry {section}.{key}");
+            throw new KeyNotFoundException(section + "." + data);
         }
     }
 }
