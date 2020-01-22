d1e540e55b5afbaf98aeaa9b0f737a03183682a2
Keithen <Keithen.Neu@gmail.com>
2020-01-23 00:54:18 +0100

    whoops

---
+++ BSU.Core/Sync/ReferenceCounter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BSU.Core.Sync
+{
+    internal class ReferenceCounter
+    {
+        private int _counter = 0;
+        private bool _started;
+        public bool Done { private set; get; }
+        public event Action OnDone;
+        private readonly object _lock = new object();
+
+        public void Inc()
+        {
+            lock (_lock)
+            {
+                _counter++;
+                _started = true;
+            }
+        }
+        
+        public void Dec()
+        {
+            lock (_lock)
+            {
+                _counter--;
+                if (_counter != 0 || !_started) return;
+                Done = true;
+                OnDone?.Invoke();
+            }
+        }
+    }
+}
