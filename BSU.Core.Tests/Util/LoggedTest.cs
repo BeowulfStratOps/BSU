b19a993b65c86f98589ed34f6359a0ffbaf688b5
Keithen <Keithen.Neu@gmail.com>
2022-01-08 18:13:34 +0100

    work around test loggin issue

--- BSU.Core.Tests/Util/LoggedTest.cs
+++ BSU.Core.Tests/Util/LoggedTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using NLog;
 using NLog.Config;
+using NLog.LayoutRenderers;
 using NLog.Targets;
 using Xunit.Abstractions;
 
@@ -24,20 +25,15 @@ namespace BSU.Core.Tests.Util
             public XUnitTarget(ITestOutputHelper outputHelper)
             {
                 _outputHelper = outputHelper;
-                LogManager.Setup().SetupExtensions(s =>
-                    s.RegisterLayoutRenderer("time-from-start", logEvent =>
-                    {
-                        _startTime ??= logEvent.TimeStamp;
-                        var delta = logEvent.TimeStamp - (DateTime)_startTime;
-                        return FormattableString.Invariant($"{delta.TotalSeconds:F3}");
-                    }));
-                Layout = NLog.Layouts.Layout.FromString("${time-from-start}|${level:uppercase=true}|${threadname}|${logger:shortName=true}|${message}");
+                Layout = NLog.Layouts.Layout.FromString("${level:uppercase=true}|${threadname}|${logger:shortName=true}|${message}");
             }
 
             protected override void Write(LogEventInfo logEvent)
             {
+                _startTime ??= logEvent.TimeStamp;
+                var delta = logEvent.TimeStamp - (DateTime)_startTime;
                 var message = Layout.Render(logEvent);
-                _outputHelper.WriteLine(message);
+                _outputHelper.WriteLine(FormattableString.Invariant($"{delta.TotalSeconds:F3}|{message}"));
             }
         }
     }
