3a1d4816290bc093578f03d81b86a05bdc55a710
Keithen <Keithen.Neu@gmail.com>
2022-02-07 23:25:08 +0100

    show preset state better

---
+++ BSU/Converters/CalculatedStateToColorBrushConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+using BSU.Core.Model;
+
+namespace BSU.GUI.Converters;
+
+[ValueConversion(typeof(ModActionEnum), typeof(Brush))]
+public class CalculatedStateToColorBrushConverter : IValueConverter
+{
+    public object Convert(object objValue, Type targetType, object parameter, CultureInfo culture)
+    {
+        var value = (CalculatedRepositoryStateEnum)objValue;
+        var color = value switch
+        {
+            CalculatedRepositoryStateEnum.NeedsSync => Colors.Blue,
+            CalculatedRepositoryStateEnum.Ready => Colors.Green,
+            CalculatedRepositoryStateEnum.RequiresUserIntervention => Colors.Red,
+            CalculatedRepositoryStateEnum.Syncing => Colors.DimGray,
+            CalculatedRepositoryStateEnum.Loading => Colors.DimGray,
+            CalculatedRepositoryStateEnum.ReadyPartial => Colors.Orange,
+            CalculatedRepositoryStateEnum.Error => Colors.Red,
+            _ => throw new ArgumentOutOfRangeException()
+        };
+        return new SolidColorBrush(color);
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
