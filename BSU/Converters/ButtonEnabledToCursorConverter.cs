5a8fa9a93ffcba41e4895004a408a3aab58404e8
Keithen <Keithen.Neu@gmail.com>
2022-02-23 12:19:52 +0100

    whoops

---
+++ BSU/Converters/ButtonEnabledToCursorConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Input;
+
+namespace BSU.GUI.Converters;
+
+public class ButtonEnabledToCursorConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return (bool)value ? Cursors.Hand : Cursors.Arrow;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ BSU/Converters/EnableToColorConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace BSU.GUI.Converters;
+
+public class EnableToColorConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        var color = (bool)value ? Colors.Black : Colors.Gray;
+        return new SolidColorBrush(color);
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
