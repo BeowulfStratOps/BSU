7b577ca0c8de9043390bac56adae5f1806b1042c
Keithen <Keithen.Neu@gmail.com>
2022-03-12 20:59:17 +0100

    forgot to add the icon

---
+++ BSU/Resources/AppIcon.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Drawing;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Markup;
+using System.Windows.Media.Imaging;
+
+namespace BSU.GUI.Resources;
+
+public class AppIcon : MarkupExtension
+{
+    public override object ProvideValue(IServiceProvider serviceProvider)
+    {
+        using var ico = Icon.ExtractAssociatedIcon(Assembly.GetExecutingAssembly().Location);
+        return Imaging.CreateBitmapSourceFromHIcon(ico.Handle, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+    }
+}
