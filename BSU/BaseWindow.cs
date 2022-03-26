38eb9f93ea8d075e0810f0ffaf875880fdcf4144
Keithen <Keithen.Neu@gmail.com>
2022-03-26 23:08:01 +0100

    made popup windows customizable

---
+++ BSU/BaseWindow.cs
@@ -0,0 +1,58 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace BSU.GUI;
+
+public class BaseWindow : Window
+{
+    public static readonly DependencyProperty TitleBarContentProperty =
+        DependencyProperty.Register("TitleBarContent", typeof(object), typeof(MainWindow), null);
+
+    public object TitleBarContent
+    {
+        get => GetValue(TitleBarContentProperty);
+        set => SetValue(TitleBarContentProperty, value);
+    }
+
+
+    public BaseWindow()
+    {
+        CommandBindings.Add(new CommandBinding(SystemCommands.CloseWindowCommand, OnCloseWindow));
+        CommandBindings.Add(new CommandBinding(SystemCommands.MaximizeWindowCommand, OnMaximizeWindow,
+            CanResizeWindow));
+        CommandBindings.Add(new CommandBinding(SystemCommands.MinimizeWindowCommand, OnMinimizeWindow,
+            CanMinimizeWindow));
+        CommandBindings.Add(new CommandBinding(SystemCommands.RestoreWindowCommand, OnRestoreWindow,
+            CanResizeWindow));
+    }
+
+    private void CanResizeWindow(object sender, CanExecuteRoutedEventArgs e)
+    {
+        e.CanExecute = ResizeMode is ResizeMode.CanResize or ResizeMode.CanResizeWithGrip;
+    }
+
+    private void CanMinimizeWindow(object sender, CanExecuteRoutedEventArgs e)
+    {
+        e.CanExecute = ResizeMode != ResizeMode.NoResize;
+    }
+
+    private void OnCloseWindow(object target, ExecutedRoutedEventArgs e)
+    {
+        SystemCommands.CloseWindow(this);
+    }
+
+    private void OnMaximizeWindow(object target, ExecutedRoutedEventArgs e)
+    {
+        SystemCommands.MaximizeWindow(this);
+    }
+
+    private void OnMinimizeWindow(object target, ExecutedRoutedEventArgs e)
+    {
+        SystemCommands.MinimizeWindow(this);
+    }
+
+    private void OnRestoreWindow(object target, ExecutedRoutedEventArgs e)
+    {
+        SystemCommands.RestoreWindow(this);
+    }
+}
--- BSU/MainWindow.xaml.cs
+++ BSU/MainWindow.xaml.cs
@@ -13,51 +13,7 @@ namespace BSU.GUI
         public MainWindow()
         {
             TitleBarContent = new Menu(); // no idea how to this from xaml
-
-            CommandBindings.Add(new CommandBinding(SystemCommands.CloseWindowCommand, OnCloseWindow));
-            CommandBindings.Add(new CommandBinding(SystemCommands.MaximizeWindowCommand, OnMaximizeWindow, OnCanResizeWindow));
-            CommandBindings.Add(new CommandBinding(SystemCommands.MinimizeWindowCommand, OnMinimizeWindow, OnCanMinimizeWindow));
-            CommandBindings.Add(new CommandBinding(SystemCommands.RestoreWindowCommand, OnRestoreWindow, OnCanResizeWindow));
             InitializeComponent();
-        }
-
-        private void OnCanResizeWindow(object sender, CanExecuteRoutedEventArgs e)
-        {
-            e.CanExecute = ResizeMode is ResizeMode.CanResize or ResizeMode.CanResizeWithGrip;
-        }
-
-        private void OnCanMinimizeWindow(object sender, CanExecuteRoutedEventArgs e)
-        {
-            e.CanExecute = this.ResizeMode != ResizeMode.NoResize;
-        }
-
-        private void OnCloseWindow(object target, ExecutedRoutedEventArgs e)
-        {
-            SystemCommands.CloseWindow(this);
-        }
-
-        private void OnMaximizeWindow(object target, ExecutedRoutedEventArgs e)
-        {
-            SystemCommands.MaximizeWindow(this);
-        }
-
-        private void OnMinimizeWindow(object target, ExecutedRoutedEventArgs e)
-        {
-            SystemCommands.MinimizeWindow(this);
-        }
-
-        private void OnRestoreWindow(object target, ExecutedRoutedEventArgs e)
-        {
-            SystemCommands.RestoreWindow(this);
-        }
-
-        public static readonly DependencyProperty TitleBarContentProperty =
-            DependencyProperty.Register("TitleBarContent", typeof(object), typeof(MainWindow), null);
-
-        public object TitleBarContent
-        {
-            get => GetValue(TitleBarContentProperty);
-            set => SetValue(TitleBarContentProperty, value);
         }
 
         private void OnMouseDown(object sender, MouseButtonEventArgs e)
